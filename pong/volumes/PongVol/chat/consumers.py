413d9d267aa686df57b6f8aa3a77e8bbd0f5ae33
boulhoujjat el mahdi <60227776+boulhoujjatmehdi@users.noreply.github.com>
2024-08-14 11:13:04 +0100

    merged

--- pong/volumes/PongVol/chat/consumers.py
+++ pong/volumes/PongVol/chat/consumers.py
@@ -1,69 +1,96 @@
 import json
-
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth.models import User
-from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from .models import Message
 from django.utils import timezone
+from users.models import User
 
-from django.contrib.sessions.models import Session
-from channels.db import database_sync_to_async
+
+from django.db.models import Q
+
+class GetLastMessage(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.sender = self.scope["url_route"]["kwargs"]["sender"]
+        self.receiver = self.scope["url_route"]["kwargs"]["receiver"]
+        self.room_group_name = f"chat_{self.sender}_{self.receiver}"
+        
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+        await self.chat_message({})
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+  
+    async def chat_message(self, event):
+        message = await self.get_message()
+        if message:
+            await self.send(text_data=json.dumps(message))
+
+    @database_sync_to_async
+    def get_message(self):
+        message = Message.objects.filter(
+            Q(sender=self.sender, receiver=self.receiver) | 
+            Q(sender=self.receiver, receiver=self.sender)
+        ).order_by('time').last()
+
+        if message:
+            return {
+                "content": message.content, 
+            }
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
+        self.sender = self.scope["url_route"]["kwargs"]["sender"]
+        self.receiver = self.scope["url_route"]["kwargs"]["receiver"]
+        self.room_group_name = f"chat_{self.sender}_{self.receiver}"
         
-        # get username
-        session_key = self.scope['cookies'].get('sessionid', None)
-        if session_key:
-            session = await database_sync_to_async(Session.objects.get)(session_key=session_key)
-            user_id = session.get_decoded().get('_auth_user_id')
-            user = await database_sync_to_async(User.objects.get)(id=user_id)
-            self.username = user.username
-        else:
-            self.username = 'Anonymous'
-
-
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
-
-        messages = await self.get_messages()
-        for message, username in messages:
-            await self.send(text_data=json.dumps({"message": message,"sender": username, "name": self.username }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        content = text_data_json["content"]
+        sender_id = text_data_json["sender"]
+        receiver_id = text_data_json["receiver"]
 
-        await self.save_message(self.username , message)
+        await self.save_message(sender_id, receiver_id, content)
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'content': content,
+                'sender': sender_id,
+                'receiver': receiver_id,
+                'time': str(timezone.now()),
+            }
         )
-    @sync_to_async
-    def save_message(self, username, message):
-        user = User.objects.get(username=username)
-        Message.objects.create(sender=user, content=message, time=timezone.now())
 
-
+    @database_sync_to_async
+    def save_message(self, sender_id, receiver_id, content):
+        sender = User.objects.get(id=sender_id)
+        receiver = User.objects.get(id=receiver_id)
+        Message.objects.create(sender=sender, receiver=receiver, content=content, time=timezone.now())
+    
     async def chat_message(self, event):
-        content, username = await self.get_message()
-        await self.send(text_data=json.dumps({"message": content, "sender": username, "name": self.username}))
+        message = await self.get_message()
+        if message:
+            await self.send(text_data=json.dumps(message))
 
-    @sync_to_async
+    @database_sync_to_async
     def get_message(self):
-        message = Message.objects.order_by('-time').first()
-        if message is None:
-            return "", ""
-        return message.content, message.sender.username
+        message = Message.objects.filter(
+            Q(sender=self.sender, receiver=self.receiver) | 
+            Q(sender=self.receiver, receiver=self.sender)
+        ).order_by('time').last()
 
-    @sync_to_async
-    def get_messages(self):
-        messages = Message.objects.order_by('time')
-        return [(message.content, message.sender.username) for message in messages]
-
-
+        if message:
+            return {
+                "content": message.content, 
+                "sender": message.sender.id, 
+                "receiver": message.receiver.id, 
+                "time": str(message.time), 
+            }

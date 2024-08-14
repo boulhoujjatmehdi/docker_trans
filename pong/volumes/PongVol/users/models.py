a5c5decac7287902aef4b444b6a1ea192cbe9b58
boulhoujjat el mahdi <60227776+boulhoujjatmehdi@users.noreply.github.com>
2024-08-14 11:41:49 +0100

    migrate

--- pong/volumes/PongVol/users/models.py
+++ pong/volumes/PongVol/users/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True, default='default_name')
     
 
     USERNAME_FIELD = 'username'
